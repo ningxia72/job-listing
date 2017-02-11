ef74faa500402c5e4521576bbfddd6552241f5d3
ningxia72 <ningxia72@gmail.com>
2017-02-11 16:38:34 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,16 +15,17 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
+
     if @job.save
-      redirect_to jobs-path
+      redirect_to jobs_path
     else
       render :new
     end
   end
 
-def edit
-  @job = Job.find(params[:id])
-end
+  def edit
+      @job = Job.find(params[:id])
+    end
 
 def update
   @job = Job.find(params[:id])
@@ -46,6 +47,6 @@ end
    private
 
    def job_params
-     params.require(:job).permit(:title, :description)
+     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
    end
 end
